53a3951190ca1825a213046f500edc9426e8c334
oscarmartensson <oscar.mattias.martensson@hotmail.com>
2019-04-19 20:41:26 +0200

    Ray tracing in one weekend
    First commit of code from the "Ray tracing in one weekend" tutorial for creating a very basic ray tracer

---
+++ ray-tracer/ray-tracer/ray-tracer/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+int main() 
+{
+    // Create a file and redirect std::cout to the file
+    std::ofstream out("test.ppm");
+    std::streambuf *coutbuf = std::cout.rdbuf();
+    std::cout.rdbuf(out.rdbuf());
+
+    // x and y dimensions
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3" << std::endl << nx << " " << ny << std::endl << 255 << std::endl;
+
+    // Write colors to file in the .ppm format
+    for (int j = ny - 1; j >= 0; j--) 
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            float r = static_cast<float>(i) / static_cast<float>(nx);
+            float g = static_cast<float>(j) / static_cast<float>(ny);
+            float b = 0.2f;
+            int ir = static_cast<int>(255.99 * r);
+            int ig = static_cast<int>(255.99 * g);
+            int ib = static_cast<int>(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << std::endl;
+        }
+    }
+
+    return 0;
+}
