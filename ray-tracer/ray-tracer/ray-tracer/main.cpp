23df8fdb6acfbeba06ad23c6c9fdba251159dad2
oscarmartensson <oscar.mattias.martensson@hotmail.com>
2019-05-12 16:33:54 +0200

    Diffuse
    Adding diffuse property to objects

--- ray-tracer/ray-tracer/ray-tracer/main.cpp
+++ ray-tracer/ray-tracer/ray-tracer/main.cpp
@@ -10,14 +10,45 @@
 #include "Camera.h"
 #include "Sphere.h"
 
+// ------Constants------
+const float tolerance = 0.001f; // For getting rid of numerical error causing shadow acne
+
+// x and y dimensions (pixels) of the output image
+const int nx = 200;
+const int ny = 100;
+
+// Anti-aliasing samples
+const int ns = 100;
+
+// Returns a pseudo-random float in range [min, max], normal distributed
+float randomFloat(float min, float max)
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_real_distribution<float> distribution(min, max);
+    return distribution(generator);
+}
+
+// Returns a random point inside a unit sphere (radius = 1)
+Vec3 randomPointInUnitSphere()
+{
+    Vec3 point;
+    do {
+        point = 2.0f * Vec3(randomFloat(0.0f, 0.9999f), randomFloat(0.0f, 0.9999f), randomFloat(0.0f, 0.9999f)) - Vec3(1.0f, 1.0f, 1.0f);
+    } while (point.squareLength() >= 1.0f);
+    return point;
+}
+
 // Returns a color of different objects that were hit with ray
 Vec3 color(const Ray& r, Hitable* world)
 {
     HitRecord record;
-    if (world->hit(r, 0.0f, FLT_MAX, record))
+    if (world->hit(r, tolerance, FLT_MAX, record))
     {
-        // If sphere front side (the side with normals pointing towards camera) was hit with rays, calculate normal and interpolate color
-        return 0.5f * Vec3(record.normal.x() + 1, record.normal.y() + 1, record.normal.z() + 1);
+        // Create new direction vector for which the ray shall bounce (diffuse material implementation)
+        Vec3 target = record.p + record.normal + randomPointInUnitSphere();
+        // Call the function again recursively with the record point and the new direction coming from diffuse "term"
+        return 0.5f * color( Ray(record.p, target - record.p), world);
     }
     else
     {
@@ -28,35 +59,19 @@ Vec3 color(const Ray& r, Hitable* world)
     }
 }
 
-// Returns a pseudo-random float in range [min, max], normal distributed
-float randomFloat(float min, float max)
-{
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::uniform_real_distribution<> distribution(min, max);
-    return distribution(generator);
-}
-
 int main() 
 {
     // Create a file and redirect std::cout to the file
     std::ofstream out("scene.ppm");
     std::streambuf *coutbuf = std::cout.rdbuf();
     std::cout.rdbuf(out.rdbuf());
-
-    // x and y dimensions
-    const int nx = 200;
-    const int ny = 100;
-    const int ns = 100;
     std::cout << "P3" << std::endl << nx << " " << ny << std::endl << 255 << std::endl;
 
+    Camera camera;
     Hitable* list[2];
     list[0] = new Sphere(Vec3(0.0f, 0.0f, -1.0f), 0.5f);
     list[1] = new Sphere(Vec3(0.0f, -100.5f, -1.0f), 100.0f);
     Hitable* world = new HitableList(list,2);
-
-    Camera camera;
-
 
     // Write colors to file in the .ppm format
     for (int j = ny - 1; j >= 0; j--) 
@@ -76,6 +91,7 @@ int main()
             }
             
             col /= static_cast<float>(ns); // Average out since we ran ns more times per pixel to anti-alias
+            col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); // Gamma correction with gamma 2 (i.e. x^(1/gamma)) since image viewer assumes some correction, otherwise very dark result
             int ir = static_cast<int>(255.99 * col[0]);
             int ig = static_cast<int>(255.99 * col[1]);
             int ib = static_cast<int>(255.99 * col[2]);
