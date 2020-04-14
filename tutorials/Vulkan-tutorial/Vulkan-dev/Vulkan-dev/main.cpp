3e3a3c05681f4ffea2bc7161eee748b22d0ab812
Oscar Mårtensson <oscar.martensson@sensegraphics.com>
2020-04-14 14:46:11 +0200

    - Added loading of .obj models

--- tutorials/Vulkan-tutorial/Vulkan-dev/Vulkan-dev/main.cpp
+++ tutorials/Vulkan-tutorial/Vulkan-dev/Vulkan-dev/main.cpp
@@ -1,6 +1,11 @@
 #define GLFW_INCLUDE_VULKAN
 #include <glfw/glfw3.h>
 
+// TinyObjLoader for loading .obj models
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tinyobjloader-master/tiny_obj_loader.h>
+
+// STB to load images
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb-master/stb_image.h>
 
@@ -17,17 +22,26 @@
 #include <fstream>
 #include <array>
 #include <chrono>
+#include <unordered_map>
 
 // GLM
-#define GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS           // Force use radians instead of degrees.
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE // Vulkan uses depth range 0 -> 1, while OpenGL uses -1 -> 1, which is why we switch.
+#define GLM_ENABLE_EXPERIMENTAL     // To use for hashing of the Vertex struct.
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
 
+// Window size
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
+// Synchronization limit
 const int MAX_FRAMES_IN_FLIGHT = 2;
+
+// Model path
+const std::string MODEL_PATH = "../models/chalet.obj";
+const std::string TEXTURE_PATH = "../textures/chalet.jpg";
 
 // Validation layers
 const std::vector<const char*> validationLayers = {
@@ -88,24 +102,43 @@ struct Vertex {
 
     return attributeDescriptions;
   }
+
+  bool operator==( const Vertex& other ) const {
+    // Same if pos, color and texcoord is the same.
+    return pos == other.pos && color == other.color && texCoord == other.texCoord;
+  }
+
 };
 
-const std::vector<Vertex> vertices = {
-    {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-    {{ 0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-    {{ 0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-    {{-0.5f,  0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
+// Hash function for the Vertex struct.
+namespace std {
+  template<> struct hash<Vertex> {
+    size_t operator()( Vertex const& vertex ) const {
+      return ( ( hash<glm::vec3>()( vertex.pos ) ^
+        ( hash<glm::vec3>()( vertex.color ) << 1 ) ) >> 1 ) ^
+        ( hash<glm::vec2>()( vertex.texCoord ) << 1 );
+    }
+  };
+}
 
-    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-    {{ 0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-    { {0.5f,  0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-    {{-0.5f,  0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}}
-};
+// To use for flat single texture, statue.jpg
 
-const std::vector<uint16_t> indices = {
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4
-};
+//const std::vector<Vertex> vertices = {
+//    {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+//    {{ 0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+//    {{ 0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+//    {{-0.5f,  0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
+//
+//    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+//    {{ 0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+//    { {0.5f,  0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+//    {{-0.5f,  0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}}
+//};
+
+//const std::vector<uint16_t> indices = {
+//    0, 1, 2, 2, 3, 0,
+//    4, 5, 6, 6, 7, 4
+//};
 
 struct UniformBufferObject {
   glm::mat4 model;
@@ -183,6 +216,7 @@ private:
     createTextureImage();
     createTextureImageView();
     createTextureSampler();
+    loadModel();
     createVertexBuffer();
     createIndexBuffer();
     createUniformBuffers();
@@ -1007,7 +1041,7 @@ private:
       vkCmdBindVertexBuffers( _commandBuffers[i], 0, 1, vertexBuffers, offsets );
 
       // Bind the index buffer.
-      vkCmdBindIndexBuffer( _commandBuffers[i], _indexBuffer, 0, VK_INDEX_TYPE_UINT16 );
+      vkCmdBindIndexBuffer( _commandBuffers[i], _indexBuffer, 0, VK_INDEX_TYPE_UINT32 );
 
       // Bind the descriptor sets used for the uniform buffers.
       vkCmdBindDescriptorSets( _commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, _pipelineLayout, 0, 1, &_descriptorSets[i], 0, VK_NULL_HANDLE );
@@ -1371,10 +1405,12 @@ private:
 
     std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
     float time = std::chrono::duration<float, std::chrono::seconds::period>( currentTime - startTime ).count();
+    const float rotationSpeedFactor = 0.1f;
 
     UniformBufferObject ubo = {};
     // Rotate the model around the z-axis 90 degress as time progresses.
-    ubo.model = glm::rotate( glm::mat4( 1.0f ), time * glm::radians( 90.0f ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+    ubo.model = glm::rotate( glm::mat4( 1.0f ), time * rotationSpeedFactor * glm::radians( 90.0f ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+    //ubo.model = glm::mat4( 1.0f );
 
     // Look at the geometry from "above" at 45 deg angle.
     ubo.view = glm::lookAt( glm::vec3( 2.0f, 2.0f, 2.0f ), glm::vec3( 0.0f, 0.0f, 0.0f ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
@@ -1461,7 +1497,7 @@ private:
   void createTextureImage() {
     int texWidth, texHeight, texChannels;
 #ifdef STB_IMAGE_IMPLEMENTATION
-    stbi_uc* pixels = stbi_load( "../textures/statue.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha );
+    stbi_uc* pixels = stbi_load( TEXTURE_PATH.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha );
 #else
     std::runtime_error( "Image loader not supported!") ;
 #endif // STB_IMAGE_IMPLEMENTATION
@@ -1634,10 +1670,12 @@ private:
     endSingleTimeCommands( commandBuffer );
   }
 
+  // Creates image views to be used for textures.
   void createTextureImageView() {
     _textureImageView = createImageView( _textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT );
   }
 
+  // Creates an image view with a given format, and returns it.
   VkImageView createImageView( VkImage image, VkFormat format, VkImageAspectFlags aspectFlags ) {
     VkImageViewCreateInfo viewInfo = {};
     viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
@@ -1658,6 +1696,7 @@ private:
     return imageView;
   }
 
+  // Creates a texture sampler to use for texturing.
   void createTextureSampler() {
     VkSamplerCreateInfo samplerInfo = {};
     samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
@@ -1682,6 +1721,7 @@ private:
     }
   }
 
+  // Creates image view used as depth image.
   void createDepthResources() {
     // Get the an appropriate depth format.
     VkFormat depthFormat = findDepthFormat();
@@ -1724,6 +1764,52 @@ private:
     return findSupportedFormat( { VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
                                 VK_IMAGE_TILING_OPTIMAL,
                                 VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT );
+  }
+
+  // Loads the .obj model provided at the MODEL_PATH.
+  void loadModel() {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if( !tinyobj::LoadObj( &attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str() ) ) {
+      throw std::runtime_error( warn + err );
+    }
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices = {};
+    // Iterate over all faces to collect the indices and put in the member variable.
+    for( const auto& shape : shapes ) {
+      for( const auto& index : shape.mesh.indices ) {
+        Vertex vertex = {};
+
+        vertex.pos = {
+          attrib.vertices[3 * index.vertex_index + 0],
+          attrib.vertices[3 * index.vertex_index + 1],
+          attrib.vertices[3 * index.vertex_index + 2]
+        };
+
+        vertex.texCoord = {
+          attrib.texcoords[2 * index.texcoord_index + 0],
+          // We send UV coords to the GPU (Vulkan) where the vertical coord 1.0 is interpreted as bottom.
+          // Flip to compensate for this.
+          1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+
+        };
+
+        vertex.color = { 1.0f, 1.0f, 1.0f };
+
+        if( uniqueVertices.count( vertex ) == 0 ) {
+          // If this vertex is unique, add its index to the map. This is to avoid
+          // duplicate vertices. Also add the vertex to the vertices vector.
+          uniqueVertices[vertex] = static_cast< uint32_t >( vertices.size() );
+          vertices.push_back( vertex );
+        }
+
+        // Look up the index of the vertex in the map and get the value.
+        indices.push_back( uniqueVertices[vertex] );
+      }
+    }
   }
 
   // --- Member variables ---
@@ -1769,6 +1855,10 @@ private:
   VkDeviceMemory _indexBufferMemory;
   std::vector<VkBuffer> _uniformBuffers;
   std::vector<VkDeviceMemory> _uniformBuffersMemory;
+
+  // Model variables
+  std::vector<Vertex> vertices;
+  std::vector<uint32_t> indices;
 
   // Misc
   size_t _currentFrame;
